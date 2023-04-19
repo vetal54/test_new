5bb2255826ec3546d1cca9778d521c6476ebeba2
Vitalii Dubovyk <vitaliy.dubovyk.1@gmail.com>
2023-04-19 20:47:45 +0300

    corrected code

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -2,27 +2,22 @@ package com.test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) {
     List<String> cities = List.of("London", "Nlondo", "Tokyo", "Kyoto", "Moscow", "Donlon", "Scowmo", "Liverpool");
-    List<String> copyOfCities = new ArrayList<>(cities);
-    List<List<String>> answer = new ArrayList<>();
+    Map<Integer, List<String>> answer = new HashMap<>();
     for (String city : cities) {
-      List<String> groupOfCities = new ArrayList<>();
-      for (String copyOfCity : copyOfCities) {
-        if (Arrays.equals(city.toLowerCase().chars().sorted().toArray(),
-            copyOfCity.toLowerCase().chars().sorted().toArray())) {
-          groupOfCities.add(copyOfCity);
-        }
-      }
-      if (!groupOfCities.isEmpty()) {
-        answer.add(groupOfCities);
-        copyOfCities.removeAll(groupOfCities);
-      }
+      answer.computeIfAbsent(hashCode(city), k -> new ArrayList<>()).add(city);
     }
     System.out.println(answer);
+  }
+
+  public static int hashCode(String str) {
+    return Arrays.hashCode(str.toLowerCase().chars().sorted().toArray());
   }
 }
